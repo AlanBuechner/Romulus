53a6164ce239795734e7c538878c995ab930348b
acer36627 <79729049+acer36627@users.noreply.github.com>
2021-10-19 18:39:04 -0600

    Logging has been implemented.

--- Romulus/src/Engine/Core/EntryPoint.cpp
+++ Romulus/src/Engine/Core/EntryPoint.cpp
@@ -3,19 +3,22 @@
 #include "Window.h"
 #include "Engine\Math\Math.h"
 #include "Cursor.h"
+#include "Log.h"
 
 #ifdef PLATFORM_WINDOWS
-
+	
 int main(int argc, char** argv)
 {
+	Engine::Log::Init();
 	Engine::Ref<Engine::Window> window = Engine::Window::Create(1080,720, "Romulus");
 
-	std::cout << "hello world" << std::endl;
+	std::cout << "Hello World!" << std::endl;
 
 	while (!window->IsWindowClosed())
 	{
 		window->OnUpdate();
-		std::cout << window->IsFullScreen() << window->IsMaximized() << window->IsMinimized() << std::endl;
+		//std::cout << window->IsFullScreen() << window->IsMaximized() << window->IsMinimized() << std::endl;
+		CORE_INFO("{0}, {1}, {2}", window->IsFullScreen(), window->IsMinimized(), window->IsMaximized());
 	}
 
 	return EXIT_SUCCESS;
---
+++ Romulus/src/Engine/Core/Log.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "Log.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+
+std::shared_ptr<spdlog::logger> Engine::Log::s_CoreLogger;
+std::shared_ptr<spdlog::logger> Engine::Log::s_ClientLogger;
+
+namespace Engine
+{
+	void Log::Init()
+	{
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger = spdlog::stdout_color_mt("ENGINE");
+		s_CoreLogger->set_level(spdlog::level::trace);
+
+		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger->set_level(spdlog::level::trace);
+	
+	}
+}
